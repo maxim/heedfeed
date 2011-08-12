b41363de91e8d8dc4fa37ae629a3706bf865781e
Maxim Chernyak <max@bitsonnet.com>
2011-08-12 13:50:19 -0400

    Some styles, polishings.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to [@feed, @item], notice: 'Item was successfully created.' }
+        format.html { redirect_to @feed, notice: 'Item was successfully created.' }
         format.json { render json: @item, status: :created, location: @item }
       else
         format.html { render action: "new" }
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       if @item.update_attributes(params[:item])
-        format.html { redirect_to [@feed, @item], notice: 'Item was successfully updated.' }
+        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,7 +1,10 @@
 class Feed < ActiveRecord::Base
   has_many :items, dependent: :destroy, order: 'created_at desc'
   has_many :new_items, class_name: 'Item', order: 'created_at desc', limit: 100
-  has_many :notifications, class_name: 'Item', conditions: ['read_at is not ?', nil], order: 'read_at desc'
+  has_many :notifications, class_name: 'Item',
+                           conditions: ['read_at is not ?', nil],
+                           order: 'read_at desc',
+                           limit: 100
 
 
   before_create :set_identifier
@@ -13,16 +16,14 @@ class Feed < ActiveRecord::Base
   private
 
   def set_identifier
-    new_identifier = generate_identifier
-
-    while Feed.exists?(:identifier => new_identifier)
+    begin 
       new_identifier = generate_identifier
-    end
+    end while Feed.exists?(:identifier => new_identifier)
 
     self.identifier = new_identifier
   end
 
   def generate_identifier
-    SecureRandom.hex(10)
+    SecureRandom.hex(10).encode('UTF-8')
   end
 end
