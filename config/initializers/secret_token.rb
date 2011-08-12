c20a80332f58dcacd466f011d0606d847ed4af28
Maxim Chernyak <max@bitsonnet.com>
2011-08-12 00:24:22 -0400

    Create empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heedfeed::Application.config.secret_token = 'fabc7d3eb1ec9d93300454f5d6234901d88a9b2c07740b75b7467c787a7d7b4e585ba9f209c17678353991b8237fc472cef7d62eb437f2b5af960dfbee93c36e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Heedfeed::Application.config.session_store :cookie_store, key: '_heedfeed_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Heedfeed::Application.config.session_store :active_record_store
